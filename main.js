a044b601ed847c6105e675a8408b5e9c041615e5
Melissa <melissa.r.wallis@gmail.com>
2023-03-08 16:48:46 -0600

    added sort container with input field and button

--- main.js
+++ main.js
@@ -12,24 +12,43 @@ const renderToDom = (divId, textToRender) => {
 //HTML component functions//
 const introContainer = () => {
   const domString = `
-  <img src="images/sorting-hat.png" class="img-fluid" alt="picture of the sorting hat from Harry Potter.">
-  <h2>
-  There's nothing hidden in your head
-  <br>
-  The Sorting Hat can't see,
-  <br>
-  So try me on and I will tell you
-  <br>
-  Where you ought to be...
-  </h2>
-  <button class="btn btn-primary" type="submit">Begin Sort</button>
-  `;
+  <div class="card" style="width: 18rem;">
+    <img src="images/sorting-hat.png" class="card-img-top" alt="picture of the sorting hat from Harry Potter">
+    <div class="card-body">
+      <h5 class="card-title">The Sorting Hat</h5>
+      <p class="card-text">
+      There's nothing hidden in your head
+      <br>
+      The Sorting Hat can't see,
+      <br>
+      So try me on and I will tell you
+      <br>
+      Where you ought to be...</p>
+      <a href="#" class="btn btn-primary">Begin Sort</a>
+    </div>
+  </div>`;
   renderToDom('#introContainer', domString)
 }
 
+const sortContainer = () => {
+  const domString = `
+  <div class="form-floating mb-3">
+  <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+  <label for="floatingInput">Enter Student Name</label>
+  </div>
+  <button type="button" class="btn btn-primary">Sort</button>
+  `
+  renderToDom('#sortContainer', domString)
+}
+
+//Event listeners
+//for Begin Sort button
+//for Sort button
+//modal that appears with sorting results?
 
 const startApp = () => {
   introContainer();
+  sortContainer();
 }
 
 startApp()
